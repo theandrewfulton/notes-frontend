b25f1abdc4a2c1c8c2516bf33e44d7a036897505
theandrewfulton <zenithsprings1@yahoo.com.au>
2022-02-24 19:16:50 +1100

    add loading and error states

--- src/scenes/note-details/index.jsx
+++ src/scenes/note-details/index.jsx
@@ -6,6 +6,9 @@ import { backend } from '../../data'
 export const NoteDetails = () => {
     const {id} = useParams()
     const [note, setNote] = useState("")
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(false)
+    const [errorMessage, setErrorMessage] = useState("")
     const navigate = useNavigate()
 
     // copied from create note - opportunity to make this DRYer in future?
@@ -14,22 +17,32 @@ export const NoteDetails = () => {
       useEffect(() => {
         backend.get(`/notes/${id}`)
             .then(({ data }) => setNote(data))
+            .catch((error) => {
+                setError(true)  
+                setErrorMessage(error.message)
+                })
+              .finally ((setLoading(false)))
     }, [id])
 
      // List delete method
      const deleteNote = async (id, index) => {
+        // set loading to true 
+        setLoading(true)
         try {
           // send request to backend
           await backend.delete(`/notes/${id}`)
           navigate("/")
         } catch (error) {
-        //   error message goes here
+            setError(true)  
+            setErrorMessage(error.message)
         }
   
       }
 
     return (
         <>
+        {loading && <p className="loading">Loading...</p>}
+        {error && <p className="error">{errorMessage}</p>}
          <article>
               <p>{note.title}</p>
               <p>{note.body}</p>
--- src/scenes/notes/index.jsx
+++ src/scenes/notes/index.jsx
@@ -5,13 +5,23 @@ import { backend } from '../../data'
 
 export const Notes = () => {
     const [notes, setNotes] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(false)
+    const [errorMessage, setErrorMessage] = useState("")
 
     useEffect(() => {
       backend.get('./notes')
       .then(({ data }) => setNotes(data))
+      .catch((error) => {
+        setError(true)  
+        setErrorMessage(error.message)
+        })
+      .finally ((setLoading(false)))
     }, [])
     return (
     <>
+        {loading && <p className="loading">Loading...</p>}
+        {error && <p className="error">{errorMessage}</p>}
         <Link to ="/notes/create"><button>New Note</button></Link>
         <div className="flex-container">
         {notes.map(({id, title, body}, index) => (
