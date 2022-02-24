a05401afbbc91ab22abd3a177d7e2cc7d67088cf
theandrewfulton <zenithsprings1@yahoo.com.au>
2022-02-24 22:33:47 +1100

    begin adding material-ui components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import './App.css';
+// import Roboto font
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 // import react-router
 import {
