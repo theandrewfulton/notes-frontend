90cf141dfbba9005cde6ecec224a84348710404a
theandrewfulton <zenithsprings1@yahoo.com.au>
2022-02-24 12:58:33 +1100

    read from backend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useState, useEffect } from "react"
+
+import { backend } from './data'
+
 function App() {
+  const [notes, setNotes] = useState([])
+
+  useEffect(() => {
+    backend.get('./notes')
+    .then(({ data }) => setNotes(data))
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    notes.map(({id, title, body}, index) => (
+      <article key={id}>
+        <p>{title}</p>
+        <p>{body}</p>
+      </article>
+    )
+    )
   );
 }
 
