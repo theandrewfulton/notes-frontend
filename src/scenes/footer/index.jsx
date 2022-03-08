f46f148d73447cea83c1bc113476d9f505dfc41a
Andrew Fulton <zenithsprings1@yahoo.com.au>
2022-03-08 23:47:12 +1100

    remove toolbar component

--- src/scenes/footer/index.jsx
+++ src/scenes/footer/index.jsx
@@ -1,7 +1,6 @@
 // imports for Material-ui
 import Box from '@mui/material/Box'
 import AppBar from '@mui/material/AppBar'
-import Toolbar from '@mui/material/Toolbar'
 import Link from '@mui/material/Link'
 
 
@@ -9,13 +8,11 @@ export const Footer = () => {
     return (
         <footer>
             <AppBar position="fixed" color="default" sx={{ top: 'auto', bottom: 0, minHeight: 0}}>
-                {/* <Toolbar> */}
-                    <Box margin="auto">
-                       <Link href="https://andrew-fulton.com" target="_blank" rel="noopener noreferrer" underline="hover" color="inherit">
-                        Andrew Fulton {new Date().getFullYear()}
-                        </Link>
-                    </Box>
-                {/* </Toolbar> */}
+                <Box margin="auto">
+                    <Link href="https://andrew-fulton.com" target="_blank" rel="noopener noreferrer" underline="hover" color="inherit">
+                    Andrew Fulton {new Date().getFullYear()}
+                    </Link>
+                </Box>
             </AppBar>
         </footer>
     )
