2dd914feedcaa00fce0c31b45441662934fdba64
Andrew Fulton <zenithsprings1@yahoo.com.au>
2022-03-08 23:17:58 +1100

    add basic mui components

--- src/scenes/footer/index.jsx
+++ src/scenes/footer/index.jsx
@@ -1,5 +1,19 @@
+// imports for Material-ui
+import Box from '@mui/material/Box'
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+
+
 export const Footer = () => {
     return (
-        <p>This is a footer</p>
+        <footer>
+            <AppBar position="fixed" color="default" sx={{ top: 'auto', bottom: 0}}>
+                <Toolbar>
+                    <Box margin="auto">
+                        Andrew Fulton {new Date().getFullYear()}
+                    </Box>
+                </Toolbar>
+            </AppBar>
+        </footer>
     )
 }
