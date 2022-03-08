3e259f6a683b0186a92e6fdb6ca94e627974bd8f
Andrew Fulton <zenithsprings1@yahoo.com.au>
2022-03-08 23:35:59 +1100

    add portfolio link

--- src/scenes/footer/index.jsx
+++ src/scenes/footer/index.jsx
@@ -2,6 +2,7 @@
 import Box from '@mui/material/Box'
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
+import Link from '@mui/material/Link'
 
 
 export const Footer = () => {
@@ -10,7 +11,9 @@ export const Footer = () => {
             <AppBar position="fixed" color="default" sx={{ top: 'auto', bottom: 0, minHeight: 0}}>
                 {/* <Toolbar> */}
                     <Box margin="auto">
+                       <Link href="https://andrew-fulton.com" target="_blank" rel="noopener noreferrer">
                         Andrew Fulton {new Date().getFullYear()}
+                        </Link>
                     </Box>
                 {/* </Toolbar> */}
             </AppBar>
