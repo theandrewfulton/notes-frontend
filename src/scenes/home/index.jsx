a679384b32c755525d51382b0336a0e23e9200c3
Andrew Fulton <zenithsprings1@yahoo.com.au>
2022-03-21 19:40:28 +1100

    if jwt in localstorage, navigate from homepage to notes

--- src/scenes/home/index.jsx
+++ src/scenes/home/index.jsx
@@ -1,4 +1,13 @@
+import { useEffect } from 'react'
+import { useNavigate } from "react-router-dom"
+
 export const Home = () => {
+    const navigate = useNavigate()
+    useEffect (() => {
+        if (localStorage.jwt) {
+            navigate("/notes")
+        }
+    })
     return (
         <p>This is the homepage</p>
     )
